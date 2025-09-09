01a3891cd07745049ed1ed84de31355a4b17d050
Miguel <miguelsoaresmachado.mm@gmail.com>
2025-09-09 15:17:57 -0300

    Add files via upload

---
+++ exercicio-sistema.js
@@ -0,0 +1,26 @@
+const os = require('os');
+const process = require('process');
+const sistemaOperacional = os.platform();
+let nomeSistema;
+if (sistemaOperacional === 'win32') {
+    nomeSistema = 'Windows';
+} else if (sistemaOperacional === 'darwin') {
+    nomesistema = 'macOS';
+} else if (sistemaOperacional === 'linux') {
+    nomesistema = 'Linux';
+} else {
+    nomeSistema = 'Sistema Desconhecido';
+}
+const memoriaBytes = os.totalmem();
+const memoriaGB = (memoriaBytes / (1024 * 1024 * 1024)).toFixed(2);
+const numeroCPUs = os.cpus().lenght;
+const tempoLigadoSegundos = os.uptime();
+const horas = Math.floor(tempoLigadoSegundos / 3600);
+const minutos = Math.floor((tempoLigadoSegundos % 3600) / 60);
+const nomeUsuario = os.userInfo().username;
+console.log('🖥️ === INFORMAÇÕES DO SISTEMA ===\n');
+console.log('💻 Sistema Operacional:', nomeSistema);
+console.log('🧠 Memória RAM Total:', memoriaGB, 'GB');
+console.log('⚡ Processadores (CPUs):', numeroCPUs);
+console.log('👤 Usuário:', nomeUsuario);
+console.log('\n✅ Análise concluída com sucesso!');
---
+++ index.js
@@ -0,0 +1,5 @@
+const colors = require('colors');
+
+console.log('🍞 Bem vindo à padaria do João!'.rainbow);
+console.log('Pães frescos todos os dias!' .green);
+console.log('Horário: 6h às 18h' .blue);
